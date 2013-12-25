425b85ce95899a18eda255a1d54ba648873ecec1
Leeviana Gray <leeviana@gmail.com>
2013-12-24 22:45:17 -0600

    Basic mongo/django interaction

---
+++ mongobacked/DataEntry/models.py
@@ -0,0 +1,11 @@
+from mongoengine import *
+from mongobacked.settings import DBNAME
+
+connect(DBNAME)
+
+class patient(Document):
+    name = StringField(max_length=120, required=True)
+    medications = DictField()
+    dispenserID = StringField(max_length=100)
+    creationTime = DateTimeField(required=True)
+    dispensed = BooleanField()
