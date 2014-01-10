84ca2e7457c0feff62362b4c6565869197e88a1d
Leeviana Gray <leeviana@gmail.com>
2014-01-10 01:11:43 -0500

    Added Medication Info Querying proof of concept... page

--- mongobacked/mongobacked/DataEntry/views.py
+++ mongobacked/mongobacked/DataEntry/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import patient
 import datetime
+import urllib2
+import json
 
 # a lot of this was taken from a sample 'blongo' project
 def index(request):
@@ -37,3 +39,37 @@ def update(request):
       params = {'patient':Patient}
 
    return render_to_response(template, params, context_instance=RequestContext(request))
+   
+def medinfo(request):
+   if 'requestType' in request.GET:
+      if request.GET['requestType'] == 'getNameSuggestions':
+         name = request.GET['medName']
+         url = ''.join(['http://rxnav.nlm.nih.gov/REST/spellingsuggestions?name=', name])
+         nameSuggestions = getJSON(url)
+         return render_to_response('medinfo.html', {'nameSuggestions': nameSuggestions}, 
+         context_instance=RequestContext(request))
+      elif request.GET['requestType'] == 'getMedSuggestions':
+         name = request.GET['medName']
+         url = ''.join(['http://rxnav.nlm.nih.gov/REST/drugs?name=', name])
+         medSuggestions = getJSON(url)
+         return render_to_response('medinfo.html', {'medSuggestions': medSuggestions}, 
+         context_instance=RequestContext(request))
+      elif request.GET['requestType'] == 'getMedicationInfo':
+         medID = request.GET['medID']
+         url = ''.join(['http://rxnav.nlm.nih.gov/REST/rxcui/', medID])
+         attributes = getJSON(''.join([url, '/allProperties?prop=attributes']))
+         names = getJSON(''.join([url, '/allProperties?prop=names']))
+         ndcs = getJSON(''.join([url, '/ndcs']))
+         ndc = ndcs['ndcGroup']['ndcList']['ndc'][0]
+         # dailymed = getJSON(''.join(['http://dailymed.nlm.nih.gov/dailymed/services/v1/ndc/', ndc, '/imprintdata.json']))
+         return render_to_response('medinfo.html', {'attributes': json.dumps(attributes, indent=3), 'names': names, 'ndc': ndc}, 
+         context_instance=RequestContext(request))
+   
+   return render_to_response('medinfo.html', {}, context_instance=RequestContext(request))
+                              
+def getJSON(url):
+   req = urllib2.Request(url, None, {'Accept': 'application/json'})
+   response = urllib2.urlopen(req)
+   ret = json.loads(response.read(), 'utf-8')
+   response.close()
+   return ret
--- mongobacked/mongobacked/urls.py
+++ mongobacked/mongobacked/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
    url(r'^$', 'mongobacked.DataEntry.views.index'),
+   url(r'^medinfo/', 'mongobacked.DataEntry.views.medinfo'),
    url(r'^update/', 'mongobacked.DataEntry.views.update'),
-   url(r'^delete/', 'mongobacked.DataEntry.views.delete'),
    url(r'^admin/', include(admin.site.urls)),
 )
