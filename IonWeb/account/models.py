a10a80490e784c5b94adb3067b5bd2bd87e24cf8
lws <ludi1001@gmail.com>
2014-01-20 14:17:40 -0500

    unimportant changes

--- IonWeb/account/models.py
+++ IonWeb/account/models.py
@@ -4,17 +4,17 @@ import re
 
 connect(DBNAME)
 
-class HashField(StringField):
+class Sha256HashField(StringField):
     """Represents SHA-2 hash output"""
     
-    HASH_REGEX = re.compile(r"^[0-9a-f]{40}")
+    HASH_REGEX = re.compile(r"^[0-9a-f]{64}")
     
     def validate(self, value):
-        if not HashField.SLUG_REGEX.match(value):
-            raise ValidationError("Not a valid SHA-2 hash: %s".format(value))
+        if not HashField.HASH_REGEX.match(value):
+            raise ValidationError("Not a valid SHA-256 hash: %s".format(value))
 
 class User(Document):
     """Represents a user in the system"""
     username = StringField(max_length=120, required=True)
-    password = HashField()
-    user_type = StringField()
+    password = Sha256HashField(required=True)
+    user_type = StringField(required=True)
--- IonWeb/account/views.py
+++ IonWeb/account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import User
 
-# Create your views here.
+import hashlib
+
+def login(request):
+    user = User(username==request.POST['username'])
+    if user.password == hashlib.sha256(request.POST['password']).hexdigest():
+        request.session['user_id'] = user._id
+        request.session['user_type'] = user.user_type
+        return HttpResponse("Login successful")
+    else:
+        return HttpResponse("Login failed")
+        
+
+def logout(request):
+    try:
+        del request.session['user_id']
+        del request.session['user_type']
+    except:
+        pass
+    return HttpResponse("Logged out")
