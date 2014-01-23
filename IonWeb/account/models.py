561d5cc5e13c2a44be59320be6ced54b8991f23a
lws <ludi1001@gmail.com>
2014-01-22 19:16:24 -0500

    working authentication

--- IonWeb/account/models.py
+++ IonWeb/account/models.py
@@ -1,24 +1,5 @@
 from mongoengine import *
-import re
 
-#connect(DBNAME)
-
-class Sha256HashField(StringField):
-    """Represents SHA-2 hash output"""
-    
-    HASH_REGEX = re.compile(r"^[0-9a-f]{64}")
-    
-    def validate(self, value):
-        if not HashField.HASH_REGEX.match(value):
-            raise ValidationError("Not a valid SHA-256 hash: %s".format(value))
-
-class User(Document):
-    """Represents a user in the system"""
-    username = StringField(max_length=120, required=True)
-    password = Sha256HashField(required=True)
-    user_type = StringField(required=True)
-    
-import mongoengine.django.mongo_auth.models
-import mongoengine.django.auth as auth
-class BetterUser(auth.User):
-    group = StringField()
+class IonUser(Document):
+    user = ReferenceField('User', reverse_delete_rule=CASCADE)
+    group = StringField(r'patient|caretaker|admin|dispenser')
--- IonWeb/account/views.py
+++ IonWeb/account/views.py
@@ -9,30 +9,23 @@ from django.contrib.auth import authenticate
 import django.contrib.auth as auth
 from django.contrib.auth.decorators import login_required
 
-import hashlib
-"""
-def login(request):
-    user = User(username==request.POST['username'])
-    if user.password == hashlib.sha256(request.POST['password']).hexdigest():
-        request.session['user_id'] = user._id
-        request.session['user_type'] = user.user_type
-        return HttpResponse("Login successful")
-    else:
-        return HttpResponse("Login failed")
-"""
+from models import IonUser
 
 def create(request):
-    user = User.create_user('john', 'password', 'lennon@thebeatles.com')
+    user = User.create_user('j', 'password', 'lennon@thebeatles.com')
+    user.groups = ['patient']
     user.save()
+    ion_user = IonUser(user=user, group='patient')
+    ion_user.save()
     return HttpResponse("Account create successful")
     
 def login(request):
-    user = authenticate(username='john', password='password')
+    user = authenticate(username='j', password='password')
     auth.login(request, user)
     if user is None:
         return HttpResponse("Login failed")
     else:
-        return HttpResponse("Login successful")
+        return HttpResponse("Login successful: " + IonUser.objects(user=user)[0].group)
         
 @login_required
 def restricted(request):
