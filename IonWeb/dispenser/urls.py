d58edb1ea195f58fc35aafb90d8e8023894147d4
ludi1001 <ludi1001@gmail.com>
2014-04-29 20:25:01 -0400

    some css stuff, dispenser page in progress, dispenser arduino code updates

--- IonWeb/dispenser/urls.py
+++ IonWeb/dispenser/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('dispenser.views',
     url(r'^$', 'management'),
     url(r'^view$','dispenser_view'),
     url(r'^admin$','dispenser_admin'),
+    url(r'^dispense-medication$', 'dispense_medication')
 )
